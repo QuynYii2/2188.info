376d4b56ad1a05d9c9901e9cada145beddd91ba1
dainq <97323194+dainqd@users.noreply.github.com>
2023-06-07 14:13:43 +0700

    Fix login google + Add send mail to account.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\NotificationStatus;
+use App\Enums\PermissionUserStatus;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Libraries\GeoIP;
+use App\Models\Notification;
+use App\Models\Permission;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 use Laravel\Socialite\Facades\Socialite;
 use function Symfony\Component\String\u;
@@ -88,8 +97,15 @@ class AuthController extends Controller
     public function loginCallback(Request $request)
     {
         try {
+            $ipAddress = $request->ip();
+            $geoIp = new GeoIP();
+            $locale = $geoIp->get_country_from_ip('183.80.130.4');
+
             $googleUser = Socialite::driver('google')->stateless()->user();
             $existingUser = User::where('email', $googleUser->email)->first();
+
+            $password = (new HomeController())->generateRandomString(8);
+            $passwordHash = Hash::make($password);
 
             if ($existingUser) {
                 auth()->login($existingUser, true);
@@ -99,20 +115,53 @@ class AuthController extends Controller
                 $newUser->provider_id = $googleUser->getId();
                 $newUser->name = $googleUser->getName();
                 $newUser->email = $googleUser->getEmail();
-                $newUser->phone = "social";
-                $newUser->address = "social";
-                $newUser->region = "social";
-                $newUser->type_account = "social";
+                $newUser->phone = "null";
+                $newUser->address = "null";
+                $newUser->region = $locale;
+                $newUser->password = $passwordHash;
+                $newUser->type_account = "null";
                 $newUser->email_verified_at = now();
                 $newUser->image = $googleUser->getAvatar();
 
                 $newUser->save();
                 auth()->login($newUser, true);
+
+                $defaultPermission1 = Permission::where('name', 'view_all_products')->first();
+                $defaultPermission2 = Permission::where('name', 'view_profile')->first();
+
+                $permissionUser1 = [
+                    'user_id' => Auth::user()->id,
+                    'created_at' => Carbon::now()->addHours(7),
+                    'permission_id' => $defaultPermission1->id,
+                    'status' => PermissionUserStatus::ACTIVE
+                ];
+
+                $permissionUser2 = [
+                    'user_id' => Auth::user()->id,
+                    'created_at' => Carbon::now()->addHours(7),
+                    'permission_id' => $defaultPermission2->id,
+                    'status' => PermissionUserStatus::ACTIVE
+                ];
+
+                DB::table('permission_user')->insert($permissionUser1);
+                DB::table('permission_user')->insert($permissionUser2);
+
+                $email = $googleUser->getEmail();
+                $data = array('mail' => $email, 'name' => $email, 'password' => $password);
+
+                Mail::send('frontend/widgets/mailWelcome', $data, function ($message) use ($email) {
+                    $message->to($email, 'Welcome mail!')->subject
+                    ('Welcome mail');
+                    $message->from('supprot.ilvietnam@gmail.com', 'Support IL');
+                });
+
+                (new HomeController())->notifiCreate(Auth::user()->id, 'Đăng kí tài khoản thành công', 'Đăng kí tài khoản');
+                (new HomeController())->notifiCreate(Auth::user()->id, 'Vui lòng cập nhập mật khẩu cho tài khoản', 'Đăng kí tài khoản');
             }
 
             $request->session()->put('login', $googleUser);
             $login = $request->session()->get('login');
-            (new HomeController())->getLocale($request);
+
             return redirect()->route('home');
 
         } catch (\Exception $exception) {
