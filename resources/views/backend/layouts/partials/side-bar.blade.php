1810eb363ef5d2b0b1beae6620830e017f1d4c5e
kiennt <kienhue98@gmail.com>
2023-06-09 15:58:54 +0700

    fix giao diện admin

--- resources/views/backend/layouts/partials/side-bar.blade.php
+++ resources/views/backend/layouts/partials/side-bar.blade.php
@@ -7,15 +7,14 @@
     }
 
     a {
-        color: #646464;
+        color: #FFFFFF;
         text-decoration: none;
     }
 
     .wrapper {
-        /*position: absolute;*/
         width: 100%;
         height: 100%;
-        background-color: #fff;
+        background-color: #000000;
         font-size: 0.875em;
     }
 
@@ -113,117 +112,6 @@
 
 </style>
 
-<aside class="sidebar" id="side-bar-seller" style="display: none">
-
-    <div id="leftside-navigation" class="nano">
-        <ul class="nano-content">
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-cogs"></i>Sản phẩm<i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-
-                    <li><a href="/products">Danh sách sản phẩm</a>
-                    </li>
-                    <li><a href="/products/create">Thêm mới sản phẩm</a>
-                    </li>
-                    <li><a href="/categories">Chuyên mục</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-table"></i>Đơn hàng<i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="#!">Basic Tables</a>
-                    </li>
-
-                    <li><a href="#!">Data Tables</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa fa-tasks"></i>Quản lý kho<i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="#!">Components</a>
-                    </li>
-                    <li><a href="#!">Validation</a>
-                    </li>
-                    <li><a href="#!">Mask</a>
-                    </li>
-                    <li><a href="#!">Wizard</a>
-                    </li>
-                    <li><a href="#!">Multiple File Upload</a>
-                    </li>
-                    <li><a href="#!">WYSIWYG Editor</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sub-menu ">
-                <a href="#!"><i class="fa fa-envelope"></i><span>Quản lý user</span><i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="#!">Inbox</a>
-                    </li>
-                    <li><a href="#!">Compose Mail</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-bar-chart-o"></i><span>Thống kê</span><i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="#!">Chartjs</a>
-                    </li>
-                    <li><a href="#!">Morris</a>
-                    </li>
-                    <li><a href="#!">C3 Charts</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-map-marker"></i><span>Quản lý mã giảm giá</span><i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="#!">Google Map</a>
-                    </li>
-                    <li><a href="#!">Vector Map</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-text-height"></i><span>Quản lý doanh thu</span></a>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-file"></i><span>Sản phẩm xem nhiều nhất</span><i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="#!">Blank Page</a>
-                    </li>
-                    <li><a href="#!">Login</a>
-                    </li>
-                    <li><a href="#!">Sign Up</a>
-                    </li>
-                    <li><a href="#!">Calendar</a>
-                    </li>
-                    <li><a href="#!">Timeline</a>
-                    </li>
-                    <li><a href="#!">404</a>
-                    </li>
-                    <li><a href="#!">500</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="#!"><i class="fa fa-text-height"></i><span>Quản lý bình luận</span><i
-                            class="arrow fa fa-angle-right pull-right"></i></a>
-                <ul>
-                    <li><a href="{{route('seller.evaluates.index')}}">List Evaluate</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-</aside>
 
 <div class='wrapper'>
     <ul class='items'>
@@ -298,22 +186,7 @@
                 <li>
                     <a href="#!"><i class="fa fa-file"></i><span>Sản phẩm xem nhiều nhất</span><i
                                 class="arrow fa fa-angle-right pull-right"></i></a>
-                    <ul class='sub-items'>
-                        <li><a href="#!">Blank Page</a>
-                        </li>
-                        <li><a href="#!">Login</a>
-                        </li>
-                        <li><a href="#!">Sign Up</a>
-                        </li>
-                        <li><a href="#!">Calendar</a>
-                        </li>
-                        <li><a href="#!">Timeline</a>
-                        </li>
-                        <li><a href="#!">404</a>
-                        </li>
-                        <li><a href="#!">500</a>
-                        </li>
-                    </ul>
+
                 </li>
                 <li>
                     <a href="#!"><i class="fa fa-text-height"></i><span>Quản lý bình luận</span><i
@@ -333,10 +206,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <script>
-    // $("#leftside-navigation .sub-menu > a").click(function (e) {
-    //     $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
-    //         e.stopPropagation();
-    // })
 
     $(".items > li > a").click(function (e) {
         e.preventDefault();
