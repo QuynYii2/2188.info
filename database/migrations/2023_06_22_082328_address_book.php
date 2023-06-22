79960968dfd12ce3b2ea33253047dd8a7a610183
kiennt <kienhue98@gmail.com>
2023-06-22 16:28:40 +0700

    tạo migration cho sổ địa chỉ

--- database/migrations/2023_06_22_082328_address_book.php
+++ database/migrations/2023_06_22_082328_address_book.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 class AddressBook extends Migration
 {
     /**
@@ -14,7 +13,15 @@ class AddressBook extends Migration
     public function up()
     {
         Schema::table('address_book', function (Blueprint $table) {
-            //
+            $table->string('fullname');
+            $table->string('company')->nullable();
+            $table->string('number_phone');
+            $table->string('province');
+            $table->string('district');
+            $table->string('wards');
+            $table->string('detail');
+            $table->string('type');
+            $table->string('default');
         });
     }
 
