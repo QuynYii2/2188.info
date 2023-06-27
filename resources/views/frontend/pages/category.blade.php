4c7d1cc35f8f9e048cafe52b637480e270038f05
kiennt <kienhue98@gmail.com>
2023-06-27 09:31:53 +0700

    fix phân trang category

--- resources/views/frontend/pages/category.blade.php
+++ resources/views/frontend/pages/category.blade.php
@@ -500,15 +500,11 @@
                             </div>
                         @endforeach
                     </div>
-                    @dd($listProduct->links()->e)
                     <nav class="mt-4 mb-5 d-flex justify-content-center" aria-label="Page navigation sample">
                         <ul class="pagination">
-
-                            <li class="page-item disabled"><a class="page-link" href="#!">{{ __('home.previous') }}</a></li>
-                            <li class="page-item active"><a class="page-link" href="#!">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#!">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#!">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#!">{{ __('home.next') }}</a></li>
+                            @foreach($listProduct->links()->elements[0] as  $index => $page)
+                                <li class="page-item"><a class="page-link" href="{{ $page }}">{{ $index }}</a></li>
+                            @endforeach
                         </ul>
                     </nav>
 
