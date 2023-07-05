41c4790f0200c3959798d828a1686b9831a880a7
dainq <97323194+dainqd@users.noreply.github.com>
2023-07-05 12:03:25 +0700

    Add product by views buyer and seller+admin.

--- resources/views/frontend/pages/profile/my-review.blade.php
+++ resources/views/frontend/pages/profile/my-review.blade.php
@@ -5,17 +5,24 @@
 @section('sub-content')
     <div class="row mt-2 bg-white rounded">
         <div class="row rounded pt-1 ml-5">
-            <h5>{{ __('home.my review') }}</h5>
+            <h5>Sản phẩm được xem nhiều nhất</h5>
         </div>
         <div class="border-bottom"></div>
         <div class="col-md-12">
             <div class="tab-content py-3 px-3 px-sm-0">
-                <div class="text-center">
-                    <img class="img" src="{{asset('images/empty.jpg')}}" alt="">
-                    <p>{{ __('home.you have no review') }} </p>
+                <div class="row">
+                    @foreach($products as $product)
+                        <div class="col-md-4 mb-1">
+                            <h5 class="text-center">
+                                <a class="link-hover"
+                                   href="{{route('detail_product.show', $product->id)}}"> {{$product->name}}</a>
+                            </h5>
+                            <img class="img" src="{{ asset('storage/' . $product->thumbnail) }}" alt="Product image">
+                            <div class="text-center text-danger">${{$product->price}}</div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
