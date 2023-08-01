ae408a7efcd1dc7efa75b2c8ab112c6a9b110533
dainq <97323194+dainqd@users.noreply.github.com>
2023-08-01 11:06:52 +0700

    Fix bug create.

--- resources/views/frontend/pages/shopProducts/list-by-shop.blade.php
+++ resources/views/frontend/pages/shopProducts/list-by-shop.blade.php
@@ -19,33 +19,37 @@
         <div class="container">
             <div class="product-list">
                 <div class="row">
-                    @foreach($products as $product)
-                        <div class="col-lg-4 col-sm-6">
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img class="img" src="{{ asset('storage/'.$product->thumbnail) }}" alt="">
-                                    <div class="sale pp-sale">Sale</div>
-                                    <div class="icon">
-                                        <i class="fa fa-heart-o"></i>
+                    @if(!$products->isEmpty())
+                        @foreach($products as $product)
+                            <div class="col-lg-4 col-sm-6">
+                                <div class="product-item">
+                                    <div class="pi-pic">
+                                        <img class="img" src="{{ asset('storage/'.$product->thumbnail) }}" alt="">
+                                        <div class="sale pp-sale">Sale</div>
+                                        <div class="icon">
+                                            <i class="fa fa-heart-o"></i>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="category-name">{{$product->category->name}}</div>
-                                    <a href="{{route('detail_product.show', $product->id)}}">
-                                        <h5>{{$product->name}}</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        ${{$product->price}}
+                                    <div class="pi-text">
+                                        <div class="category-name">{{$product->category->name}}</div>
+                                        <a href="{{route('detail_product.show', $product->id)}}">
+                                            <h5>{{$product->name}}</h5>
+                                        </a>
+                                        <div class="product-price">
+                                            ${{$product->price}}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
     </section>
 @endsection
-@php
-      (new \App\Http\Controllers\Frontend\HomeController())->createStatisticShopDetail('access', $product->user_id)
-@endphp
+@if(!$products->isEmpty())
+    @php
+          (new \App\Http\Controllers\Frontend\HomeController())->createStatisticShopDetail('access', $product->user_id)
+    @endphp
+@endif
