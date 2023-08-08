35b7a89c015e46de39914d135a4f724e0ccee985
kiennt <kienhue98@gmail.com>
2023-08-08 11:47:06 +0700

    fix: cache translate

--- app/Http/Controllers/TranslateController.php
+++ app/Http/Controllers/TranslateController.php
@@ -2,20 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Cache;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class TranslateController extends Controller
 {
     private static $instance = null;
     private $translate;
-    private $currentCountryCode;
 
     private function __construct()
     {
         $this->translate = new GoogleTranslate();
         $this->translate->setTarget($this->getCurrentCountryCode());
-        $this->currentCountryCode = $this->getCurrentCountryCode();
     }
 
     public static function getInstance()
@@ -28,30 +25,24 @@ class TranslateController extends Controller
 
     public function translateText($str)
     {
-        $cacheKey = 'translated_text_' . md5($str) . '_' . $this->currentCountryCode;
-
-        return Cache::remember($cacheKey, null, function () use ($str) {
-            return $this->translate->translate($str);
-        });
+        return $this->translate->translate($str);
     }
 
     function getCurrentCountryCode()
     {
-        return Cache::remember('current_country_code', null, function () {
-            $currentLocale = app()->getLocale();
+        $currentLocale = app()->getLocale();
 
-            $localeToCountryMap = [
-                'kr' => 'ko',
-                'cn' => 'zh-CN',
-                'vi' => 'vi',
-            ];
+        $localeToCountryMap = [
+            'kr' => 'ko',
+            'cn' => 'zh-CN',
+            'vi' => 'vi',
+        ];
 
-            if (isset($localeToCountryMap[$currentLocale])) {
-                return $localeToCountryMap[$currentLocale];
-            } else {
-                return 'en';
-            }
-        });
+        if (isset($localeToCountryMap[$currentLocale])) {
+            return $localeToCountryMap[$currentLocale];
+        } else {
+            return 'en';
+        }
     }
 
 }
