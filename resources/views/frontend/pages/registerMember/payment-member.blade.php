8a4f839c15f334187de66af637d3f1d3dc968964
dainq <97323194+dainqd@users.noreply.github.com>
2023-08-16 15:35:52 +0700

    Fix bug register member.

--- resources/views/frontend/pages/registerMember/payment-member.blade.php
+++ resources/views/frontend/pages/registerMember/payment-member.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     @php
 
-    @endphp
+            @endphp
     <link rel="stylesheet" href="{{asset('css/register_member.css')}}">
     <div class="start-page mb-3">
         <div class="background container-fluid pt-3 justify-content-center pb-3">
@@ -14,16 +14,16 @@
                     <div class="title">Payment Register Member</div>
                 </div>
                 <h3 class="text-center">
-                    Phí gia nhập hội viên {{ ($property->name) }}{{$registerMember}}
+                    Phí gia nhập hội viên {{$registerMember}}
                 </h3>
                 <div class="mt-5">
                     <div class="row p-5">
                         <br>
                         @php
-                                $id = 0;
-                                $memberRegister = \App\Models\MemberRegisterInfo::where([
-                                    ['user_id', $id],
-                                    ['member', $registerMember]])->orderBy('created_at', 'desc')->first();
+                            $memberRegister = \App\Models\MemberRegisterInfo::where([
+                                ['user_id', Auth::user()->id],
+                                ['member', $registerMember]])->orderBy('created_at', 'desc')->first();
+                            $member = \App\Models\Member::find($memberRegister->member_id);
                         @endphp
                         <h5>Info</h5>
                         <table class="table table-bordered">
@@ -56,7 +56,7 @@
                         </table>
                         @php
                             $memberRegisterSource = \App\Models\MemberRegisterPersonSource::where([
-                                ['user_id', $id],
+                                ['user_id', Auth::user()->id],
                                 ['member_id', $memberRegister->id],
                                 ['type', \App\Enums\MemberRegisterType::SOURCE]])->orderBy('created_at', 'desc')->first();
                         @endphp
@@ -81,7 +81,7 @@
                         </table>
                         @php
                             $memberRegisterRepresent = \App\Models\MemberRegisterPersonSource::where([
-                                ['user_id', $id],
+                                ['user_id', Auth::user()->id],
                                 ['person', $memberRegisterSource->id],
                                 ['type', \App\Enums\MemberRegisterType::REPRESENT]])->orderBy('created_at', 'desc')->first();
                         @endphp
@@ -106,10 +106,24 @@
                         </table>
                         <div class="col-md-6 border">
                             <h5 class="text-center">Quyền lợi hội viên</h5>
+                            @php
+                                $listPermissionID = $member->permission_id;
+                                $arrayPermissionID = null;
+                                if ($listPermissionID){
+                                    $arrayPermissionID = explode(',', $listPermissionID);
+                                }
+                            @endphp
                             <ol class="text-success">
-                                <li>Tìm các sản phẩm</li>
-                                <li>Quản lí và tìm kiếm giao dịch</li>
-                                <li>Nhắn tin và quảng cáo sản phẩm</li>
+                                @if($arrayPermissionID)
+                                    @foreach($arrayPermissionID as $permissionID)
+                                        <li>
+                                            @php
+                                                $permission = \App\Models\Permission::find($permissionID);
+                                            @endphp
+                                            {{$permission->name}}
+                                        </li>
+                                    @endforeach
+                                @endif
                             </ol>
                         </div>
                         <div class="col-md-6 border">
@@ -118,35 +132,12 @@
                                 <input hidden="" type="text" name="member_id" value="{{$memberRegister->id}}">
                                 <input hidden="" type="text" name="role" value="{{ ($registerMember) }}">
                                 <h5 class="text-center">Mức giá</h5>
-                                @if($registerMember == \App\Enums\RegisterMember::VENDOR)
-                                    PRICE:
-                                    <span class="text-danger" style="font-weight: 600; font-size: 36px">
-                                    $ {{\App\Enums\RegisterMemberPrice::VENDOR}}
+                                PRICE:
+                                <span class="text-danger" style="font-weight: 600; font-size: 36px">
+                                    $ {{$member->price}}
                                 </span>
-                                    <input hidden="" type="text" name="price"
-                                           value="{{\App\Enums\RegisterMemberPrice::VENDOR}}">
-                                @elseif($registerMember == \App\Enums\RegisterMember::POWER_VENDOR)
-                                    PRICE:
-                                    <span class="text-danger" style="font-weight: 600; font-size: 36px">
-                                    $ {{\App\Enums\RegisterMemberPrice::POWER_VENDOR}}
-                                </span>
-                                    <input hidden="" type="text" name="price"
-                                           value="{{\App\Enums\RegisterMemberPrice::POWER_VENDOR}}">
-                                @elseif($registerMember == \App\Enums\RegisterMember::PRODUCTION)
-                                    PRICE:
-                                    <span class="text-danger" style="font-weight: 600; font-size: 36px">
-                                    $ {{\App\Enums\RegisterMemberPrice::PRODUCTION}}
-                                </span>
-                                    <input hidden="" type="text" name="price"
-                                           value="{{\App\Enums\RegisterMemberPrice::PRODUCTION}}">
-                                @else
-                                    PRICE:
-                                    <span class="text-danger" style="font-weight: 600; font-size: 36px">
-                                    $ {{\App\Enums\RegisterMemberPrice::POWER_PRODUCTION}}
-                                </span>
-                                    <input hidden="" type="text" name="price"
-                                           value="{{\App\Enums\RegisterMemberPrice::POWER_PRODUCTION}}">
-                                @endif
+                                <input hidden="" type="text" name="price"
+                                       value="{{$member->price}}">
                                 <div class="float-right mt-5">
                                     @if($memberRegister->status == \App\Enums\MemberRegisterInfoStatus::INACTIVE)
                                         <button type="submit" class="btn btn-warning">
