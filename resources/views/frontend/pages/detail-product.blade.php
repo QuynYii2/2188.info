f6451af5928a092e686dde9dbfb356940d2b8661
kienh <kienhue98@gmail.com>
2023-08-23 16:02:11 +0700

    Fix bug oder.

--- resources/views/frontend/pages/detail-product.blade.php
+++ resources/views/frontend/pages/detail-product.blade.php
@@ -341,6 +341,7 @@
                     @endif
                     <div class="">
                         <input id="product_id" hidden value="{{$product->id}}">
+                        <input name="price" id="price" hidden value="{{$product->price}}">
                         @if(count($productDetails)>0)
                             <input name="variable" id="variable" hidden value="{{$productDetails[0]->variation}}">
                         @endif
@@ -907,6 +908,8 @@
             checkBtn();
         });
 
+        var price = document.getElementById('price')
+
         function checkBtn() {
             for (let i = 0; i < radio.length; i++) {
                 if (radio[i].checked == true) {
@@ -938,6 +941,7 @@
                 .then((response) => {
                     productThumbnail.src = urlImg + '/' + response['thumbnail'];
                     productPrice.innerText = response['price'];
+                    price.value = response['price'];
                     productOldPrice.innerText = response['old_price'];
                     productQuantity.innerText = response['quantity'];
                     variable.value = response['variation'];
