742a88460d9b2d8e2dae1f1ef4f1d81641adc6c0
Quy Nguyen <quy.nguyen@192.168.2.10>
2023-08-28 18:00:00 +0700

    create location helper

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -330,11 +330,11 @@ class HomeController extends Controller
     {
         $geoIp = new GeoIP();
         $locale = $geoIp->getCode($request->ip());
+        dd($locale);
         $countries = new Countries();
         $country = $countries->all()->pluck('name.common')->toArray();
         $currencies = $countries->all()->pluck('currencies')->toArray();
         $all = $countries->where('name.common', $locale)->first()->hydrate('currencies')->currencies;
-        dd($all);
         foreach ($all as $items) {
             $currency = $items->iso->code;
         }
