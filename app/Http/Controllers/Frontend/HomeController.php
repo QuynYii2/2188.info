3dc6a9e0fa4eb8431b9151179cf627d1542e6b6d
Quy Nguyen <quy.nguyen@192.168.2.10>
2023-08-29 17:02:23 +0700

    change to vi

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -47,7 +47,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $this->getLocale($request);
-        $locale = 'kr';
+        $locale = 'vi';
         if ($locale == 'vn') {
             $locale = 'vi';
         }
