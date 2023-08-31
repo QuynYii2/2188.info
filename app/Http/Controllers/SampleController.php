a5e915bc6962808d0f446198aaa758256d5403e9
Quy Nguyen <quy.nguyen@192.168.2.10>
2023-08-31 10:53:57 +0700

    fix translate header blade message

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -8,16 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class SampleController extends Controller
 {
-    public function chat(){
+    public function chat(Request $request){
+        (new HomeController())->getLocale($request);
         return view('frontend.pages.message.chat');
     }
 
-    public function getListMessageSent(){
+    public function getListMessageSent(Request $request){
+        (new HomeController())->getLocale($request);
         $listMessage = Chat::where('from_user_id', Auth::user()->id)->orderBy('id', 'desc')->paginate(3);
         return view('frontend.pages.message.message-sent', compact('listMessage'));
     }
 
-    public function getListMessageReceived(){
+    public function getListMessageReceived(Request $request){
+        (new HomeController())->getLocale($request);
         $listMessage = Chat::where('to_user_id', Auth::user()->id)->orderBy('id', 'desc')->paginate(3);
         return view('frontend.pages.message.message-received', compact('listMessage'));
     }
