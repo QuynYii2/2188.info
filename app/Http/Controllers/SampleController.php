d502c602fc7990ca19e95ce3943abf90e5f8fa41
Quy Nguyen <quy.nguyen@192.168.2.10>
2023-08-31 11:02:11 +0700

    remove constructor

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -2,36 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Frontend\HomeController;
 use App\Models\Chat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Frontend\HomeController;
 
 class SampleController extends Controller
 {
-    protected $locale;
-
-    public function __construct(Request $request)
-    {
-        parent::__construct();
-
-        $homeController = new HomeController();
-        $this->locale = $homeController->getLocale($request);
-    }
-
     public function chat(Request $request)
     {
+        $homeController = new HomeController();
+        $this->locale = $homeController->getLocale($request);
         return view('frontend.pages.message.chat');
     }
 
-    public function getListMessageSent()
+    public function getListMessageSent(Request $request)
     {
+        $homeController = new HomeController();
+        $this->locale = $homeController->getLocale($request);
         $listMessage = Chat::where('from_user_id', Auth::user()->id)->orderBy('id', 'desc')->paginate(3);
         return view('frontend.pages.message.message-sent', compact('listMessage'));
     }
 
-    public function getListMessageReceived()
+    public function getListMessageReceived(Request $request)
     {
+        $homeController = new HomeController();
+        $this->locale = $homeController->getLocale($request);
         $listMessage = Chat::where('to_user_id', Auth::user()->id)->orderBy('id', 'desc')->paginate(3);
         return view('frontend.pages.message.message-received', compact('listMessage'));
     }
