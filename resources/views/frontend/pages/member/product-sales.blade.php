6dbd6a0ad326f71d72d0e2883f0731f45bb88040
Quy Nguyen <quy.nguyen@Quys-MacBook-Pro.local>
2023-09-05 00:28:18 +0700

    test

--- resources/views/frontend/pages/member/product-sales.blade.php
+++ resources/views/frontend/pages/member/product-sales.blade.php
@@ -6,7 +6,8 @@
         <tr>
             <td class="12">{{$price_sale->quantity}}</td>
             <td>{{ number_format(convertCurrency('USD', $currency,($product->price - ($product->price * $price_sale->sales / 100)) * $price_sale->quantity), 0, ',', '.') }} {{$currency}}</td>
-            <td class="11111">{{$price_sale->days}} {{ __('home.Status') }}</td>
+            <td class="11111">{{$price_sale->days}} {{ __('home.ngày kể từ ngày đặt hàng') }}</td>
+            <span>{{ __('home.Close') }}</span>
         </tr>
     @endforeach
 @endif
