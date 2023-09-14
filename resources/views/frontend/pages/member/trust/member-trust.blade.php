b460f0916a5baf5e72873c7cd0141e38ffeec5ef
Quy Nguyen <quy.nguyen@192.168.2.10>
2023-09-14 10:40:32 +0700

    resize modal

--- resources/views/frontend/pages/member/trust/member-trust.blade.php
+++ resources/views/frontend/pages/member/trust/member-trust.blade.php
@@ -103,7 +103,7 @@
     </div>
     <div class="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
+            <div class="modal-content" style="width: 30%">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">{{ __('home.Chọn quốc gia mua hàng') }}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
