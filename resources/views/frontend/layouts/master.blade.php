9bd5ad380c426abc523fa0dd6271d0d71c60d349
dainqd <ngodaix5tp@gmail.com>
2023-09-26 08:58:26 +0700

    Fix js.

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -73,6 +73,7 @@ $isRoute = in_array($currentRouteName, $arrNameNeedHid);
           rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <div class="d-none">
     @if(Auth::check())
@@ -131,14 +132,9 @@ $isRoute = in_array($currentRouteName, $arrNameNeedHid);
 {{--<script src="{{ asset('js/vendor/owl.carousel.min.js') }}"></script>--}}
 {{--<script src="{{ asset('js/mainV1.js') }}"></script>--}}
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="{{ asset('js/style.js') }}"></script>
 </body>
 </html>
