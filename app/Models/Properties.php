cd80c77ffb253931e3e8bb814bd21cc95207963a
dainqd <ngodaix5tp@gmail.com>
2023-10-02 16:29:04 +0700

    Add model.

--- app/Models/Properties.php
+++ app/Models/Properties.php
@@ -9,7 +9,7 @@ class Properties extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'attribute_id', 'slug', 'description', 'status','name_vi', 'name_ja', 'name_ko', 'name_en', 'name_zh'];
+    protected $fillable = ['name', 'attribute_id', 'slug', 'description', 'status', 'name_vi', 'name_ja', 'name_ko', 'name_en', 'name_zh'];
 
     public function attribute()
     {
