a8ca314ef493fa935f28496595b6afd7d7da6206
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-03 14:20:23 +0700

    list payments method

--- resources/views/frontend/pages/checkout.blade.php
+++ resources/views/frontend/pages/checkout.blade.php
@@ -19,6 +19,7 @@
                     <form id="checkout-form" method="post">
                         @csrf
                         <div class="row">
+
                             <div class="col-md-8 col-12 checkout-item">
                                 <div class="row">
                                     <div class="col-12">
@@ -93,33 +94,6 @@
                                         <input type="radio" class="input-m0" name="order_method" id="order-by-e-wallet"
                                                 {{OrderMethod::ElectronicWallet}}/>
                                         <span class="ml-1">{{ __(OrderMethod::ElectronicWallet) }}</span>
-                                    </div>
-                                    <div class="col-12" id="payment-info">
-                                        <h3>{{ __('home.Payment') }}</h3>
-                                        <label for="fname">{{ __('home.Accepted Cards') }}</label>
-                                        <div class="icon-container">
-                                            <i class="fa fa-cc-visa" style="color:navy;"></i>
-                                            <i class="fa fa-cc-amex" style="color:blue;"></i>
-                                            <i class="fa fa-cc-mastercard" style="color:red;"></i>
-                                            <i class="fa fa-cc-discover" style="color:orange;"></i>
-                                        </div>
-                                        <label for="cname">{{ __('home.Name on Card') }}</label>
-                                        <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-                                        <label for="ccnum">{{ __('home.Card Number') }}</label>
-                                        <input type="text" id="ccnum" name="cardnumber"
-                                               placeholder="1111-2222-3333-4444">
-                                        <label for="expmonth">{{ __('home.Expiration Month') }}</label>
-                                        <input type="text" id="expmonth" name="expmonth" placeholder="September">
-                                        <div class="row">
-                                            <div class="col-50">
-                                                <label for="expyear">{{ __('home.Expiration Year') }}</label>
-                                                <input type="text" id="expyear" name="expyear" placeholder="2018">
-                                            </div>
-                                            <div class="col-50">
-                                                <label for="cvv">CVV</label>
-                                                <input type="text" id="cvv" name="cvv" placeholder="352">
-                                            </div>
-                                        </div>
                                     </div>
 
                                 </div>
