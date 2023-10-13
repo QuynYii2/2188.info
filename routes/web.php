24a0c5bafaa16b7c77c0ad157148d96d5046d897
kienh <kienhue98@gmail.com>
2023-10-13 08:57:08 +0700

    Revert "feat: tạo route cho laravel filemanager"
    This reverts commit 514fa80770cc5d2fa92b0d87493cc044317a68f8.

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
-    \UniSharp\LaravelFilemanager\Lfm::routes();
-});
 
 Route::get('/lang/kr', function ($locale) {
     session()->put('locale', 'kr');
