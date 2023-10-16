f601544ed0bed50b935505b175e2dfcb4a188ac4
kienh <kienhue98@gmail.com>
2023-10-16 14:51:12 +0700

    fix: name_en khi tạo mới địa chỉ

--- app/Http/Controllers/Admin/AdminAddressController.php
+++ app/Http/Controllers/Admin/AdminAddressController.php
@@ -93,13 +93,11 @@ class AdminAddressController extends Controller
         $code = (new HomeController())->generateRandomString(2);
 
         $name = $request->input('name');
+        $nameEN = $request->input('name_en');
         $sort_index = $request->input('sort_index');
         $status = $request->input('status');
         $isShow = $request->input('isShow') ?? 1;
         $created_by = Auth::user()->id;
-
-        $ld = new TranslateController();
-        $nameEN = $ld->translateText($name, 'en');
 
         $codeParent = $request->input('up_code');
 
