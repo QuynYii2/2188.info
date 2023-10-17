28c9f0653cbf4571c76c73b87ecf816c74db98f5
kienh <kienhue98@gmail.com>
2023-10-17 14:30:08 +0700

    chore: dịch address

--- resources/views/backend/address/index.blade.php
+++ resources/views/backend/address/index.blade.php
@@ -82,7 +82,7 @@
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">{{ __('home.Add region ') }}</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">{{ __('home.Add region') }}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
