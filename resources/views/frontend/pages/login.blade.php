5b77513fd0c5687e034061a41f9454ac12da788c
Quy Nguyen <quy.nguyen@Quys-MacBook-Pro.local>
2023-11-10 13:57:14 +0700

    fix table login

--- resources/views/frontend/pages/login.blade.php
+++ resources/views/frontend/pages/login.blade.php
@@ -97,16 +97,20 @@
                                             </button>
                                         </th>
                                     </tr>
-                                    <tr class="text-center">
-                                        <th scope="row">
-                                            <a href="{{route('register.show')}}">{{ __('home.sign up') }}</a>
-                                        </th>
-                                        <th scope="row" colspan="2">
-                                            <a href="#">{{ __('home.Find password/id Login') }}</a>
-                                        </th>
-                                    </tr>
                                     </tbody>
                                 </form>
+                            </table>
+                            <table>
+                                <tbody>
+                                <tr class="text-center">
+                                    <th scope="row">
+                                        <a href="{{route('register.show')}}">{{ __('home.sign up') }}</a>
+                                    </th>
+                                    <th scope="row" colspan="2">
+                                        <a href="#">{{ __('home.Find password/id Login') }}</a>
+                                    </th>
+                                </tr>
+                                </tbody>
                             </table>
                         </div>
                     </div>
