43e3c9a46651cc5361088fd02fcded23f7df31bf
dainqd <ngodaix5tp@gmail.com>
2023-11-20 17:40:46 +0700

    fix: show new login page.

--- resources/views/frontend/pages/login.blade.php
+++ resources/views/frontend/pages/login.blade.php
@@ -1,126 +1,95 @@
 @extends('frontend.layouts.master')
 
 @section('title', 'Login')
-
 <style>
-    .login-tags {
-        display: inline-block;
-        margin: auto;
+    .page-login {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 600px;
+        padding: 20px;
     }
 
-    .link-tabs {
-        color: #cccccc;
-        background-color: #f9f9f9 !important;
+    @media (max-width: 768px) {
+        .page-login {
+            width: 80%;
+            margin: 0 auto;
+        }
     }
 
-    .link-tabs:hover {
-        color: #c69500;
-    !important;
-        background-color: #f7f7f7;
+    .login-title {
+        font-size: 24px;
+        font-weight: 600;
     }
 
-    #tableLogin th, #tableLogin td {
-        vertical-align: middle !important;
+    .login-element {
+        font-size: 12px;
+        font-weight: 400;
+    }
+
+    .text-password-forget {
+        font-size: 16px;
+        font-weight: 600;
+    }
+
+    #btnLogin {
+        width: 240px;
+        padding: 12px 16px;
+        font-size: 16px;
+        font-weight: 600;
+        color: #fff;
+    }
+
+    .text-no-account {
+        font-size: 18px;
+    }
+
+    .text-no-account p {
+        font-weight: 400;
+    }
+
+    .text-no-account a {
+        font-weight: 600;
     }
 </style>
+
 @section('content')
     <div class="background-login">
-        <div class=" container p-5 ">
-            <div class="row p-5">
-                <div class="col-md-2">
-
-                </div>
-                <div class="col-md-8 bg bg-white p-4 rounded">
+        <div class="container p-5 ">
+            <div class="page-login">
+                <div class="bg bg-white p-4 rounded">
                     <div class="form-title text-center pt-2 mb-5 ">
-                        <div class="title">{{ __('home.sign in') }}</div>
+                        <div class="title login-title">{{ __('home.sign in') }}</div>
                     </div>
-                    <div class="row text-center">
-                        <div class="col-md-11 login-tags">
-                            <form method="post" action="{{ route('login.submit') }}" id="formLogin"
-                                  style="border: 3px solid rgb(241, 172, 139); padding: 20px;">
+                    <div class="login-form-main">
+                        <div class="login-tags">
+                            <form method="post" action="{{ route('login.submit') }}" id="formLogin">
                                 @csrf
-                                <table class="table element-bordered-pink align-middle" align="center" id="tableLogin">
-                                    <tbody>
-                                    <tr class="text-center">
-                                        <th scope="row" style="width: 160px;">
-                                            <label for="login_email_1">{{ __('home.ID') }}: </label>
-                                        </th>
-                                        <td colspan="2">
-                                            <input id="login_email_1" type="email" class="form-control"
-                                                   name="login_field"
-                                                   placeholder="{{ __('home.input username') }}"
-                                                   value="{{ old('login_field') }}" autofocus>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                <table class="table element-bordered-pink align-middle" align="center" id="tableLogin">
-                                    <tbody>
-                                    <tr class="text-center">
-                                        <th scope="row" style="width: 160px;">
-                                            <label for="login_password_1">{{ __('home.Password') }}: </label>
-                                        </th>
-                                        <td colspan="2">
-                                            <input type="password" class="form-control" name="password"
-                                                   id="login_password_1"
-                                                   placeholder="{{ __('home.input password') }}">
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-
-                                <th scope="row" colspan="3" class="solid-4x-pink" style="width: 160px;">
-                                    <button style="    width: 65%; position: relative; left: 36%; height: 75px"
-                                            type="button" onclick="submitFormLogin()"
-                                            class="btn btn-warning btn-block btn-round"
-                                            style="height: 75px">{{ __('home.sign in') }}
-                                    </button>
-                                </th>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1" class="login-element">Email:</label>
+                                    <input type="email" name="login_field" class="form-control" id="exampleInputEmail1"
+                                           aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword" class="login-element">Password:</label>
+                                    <input type="password" name="password" class="form-control"
+                                           id="exampleInputPassword">
+                                </div>
+                                <div class="float-right">
+                                    <a href="#" class="text-decoration-none text-password-forget">Forgot password?</a>
+                                </div>
+                                <div class="text-center mt-5">
+                                    <button type="submit" id="btnLogin" class="btn bg-main">Login</button>
+                                </div>
+                                <div class="text-center mt-3 text-no-account">
+                                    <p>Do not have an account?<span> <a class="text-main" href="{{ route('register.show') }}">Sign up</a></span></p>
+                                </div>
                             </form>
-                            <table class="table element-bordered-pink align-middle" align="center">
-                                <tbody>
-                                <tr class="text-center">
-                                    <th scope="row">
-                                        <a href="{{route('register.show')}}">{{ __('home.sign up') }}</a>
-                                    </th>
-                                    <th scope="row" colspan="2">
-                                        <a href="#">{{ __('home.Find password/id Login') }}</a>
-                                    </th>
-                                </tr>
-                                </tbody>
-                            </table>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-2">
-
                 </div>
             </div>
         </div>
     </div>
-    <script>
-
-        function clearLocal() {
-            localStorage.clear();
-        }
-
-        clearLocal();
-
-        function submitFormLogin() {
-            const checkForm = checkFormInput();
-            if (!checkForm) {
-                alert('Vui lòng nhập đầy đủ thông tin');
-                return;
-            }
-            document.getElementById('formLogin').submit();
-        }
-
-        function checkFormInput() {
-            const email = document.getElementById('login_email_1').value;
-            const password = document.getElementById('login_password_1').value;
-
-            return !(!email || !password);
-        }
-
-    </script>
 @endsection
