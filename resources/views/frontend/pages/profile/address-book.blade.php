67eba8bc9c0eea20e88b99618f635080bcb5fd9b
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-12-12 18:05:54 +0700

    fix: address book

--- resources/views/frontend/pages/profile/address-book.blade.php
+++ resources/views/frontend/pages/profile/address-book.blade.php
@@ -4,17 +4,17 @@
 <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
 @section('content')
     <div class="container-fluid" id="address-book">
-        <div class="row mt-2 bg-white rounded">
-            <div class="row  rounded pt-1 ml-5">
-                <h5>{{ __('home.address book') }}</h5>
+        <div class="row mt-2 bg-white rounded header-address-book">
+            <div class="row w-100 rounded d-flex justify-content-center align-items-center title-address-book">
+                {{ __('home.address book') }}
             </div>
             <div class="border-bottom"></div>
         </div>
-        <div class="row bg-white rounded mt-2" style="display: contents">
-            <button class="btn btn-outline-info bg-white py-3 link-tabs" id="addAction" data-toggle="modal"
+        <div class="row bg-white rounded mt-2 justify-content-end d-flex">
+            <button class="btn btn-outline-info py-3 link-tabs" id="addAction" data-toggle="modal"
                     data-target="#modal-address">
-                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M12 5V19M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
                 {{ __('home.add address') }}
             </button>
@@ -22,21 +22,37 @@
 
         <div class="row bg-white mt-3">
             @foreach($addresses as $address)
-                <div class="col-12 col-sm-12 my-2 border">
+                <div class="col-12 col-sm-12 my-2 border-address-book">
                     <div class="py-3">
                         <div class="d-flex justify-content-between">
-                            <h5 class="mb-1">{{ ($address->username) }}</h5>
+                            <div class="d-flex"><span class="mb-1">{{ __('home.Name') }}:&nbsp;</span><div
+                                        class="name-address-book"> {{ ($address->username) }}</div></div>
+
                             <div class="">
                                 <button class="btn btn-outline- " onclick="editModal({{$address}})"
                                         data-target="#modal-address-edit"
-                                        data-toggle="modal" style="color: blue">{{ __('home.edit') }}</button>
-                                <a href="/address/delete/{{$address->id}}"><button class="btn btn-outline- " style="color: red">{{ __('home.delete') }}</button></a>
+                                        data-toggle="modal" style="color: blue"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <path d="M12 20.0002H21M3 20.0002H4.67454C5.16372 20.0002 5.40832 20.0002 5.63849 19.945C5.84256 19.896 6.03765 19.8152 6.2166 19.7055C6.41843 19.5818 6.59138 19.4089 6.93729 19.063L19.5 6.50023C20.3285 5.6718 20.3285 4.32865 19.5 3.50023C18.6716 2.6718 17.3285 2.6718 16.5 3.50023L3.93726 16.063C3.59136 16.4089 3.4184 16.5818 3.29472 16.7837C3.18506 16.9626 3.10425 17.1577 3.05526 17.3618C3 17.5919 3 17.8365 3 18.3257V20.0002Z" stroke="#929292" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg></button>
+                                <a href="/address/delete/{{$address->id}}"><button class="btn btn-outline- " style="color: red"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path d="M16 6V5.2C16 4.0799 16 3.51984 15.782 3.09202C15.5903 2.71569 15.2843 2.40973 14.908 2.21799C14.4802 2 13.9201 2 12.8 2H11.2C10.0799 2 9.51984 2 9.09202 2.21799C8.71569 2.40973 8.40973 2.71569 8.21799 3.09202C8 3.51984 8 4.0799 8 5.2V6M10 11.5V16.5M14 11.5V16.5M3 6H21M19 6V17.2C19 18.8802 19 19.7202 18.673 20.362C18.3854 20.9265 17.9265 21.3854 17.362 21.673C16.7202 22 15.8802 22 14.2 22H9.8C8.11984 22 7.27976 22 6.63803 21.673C6.07354 21.3854 5.6146 20.9265 5.32698 20.362C5 19.7202 5 18.8802 5 17.2V6" stroke="#E90000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg></button></a>
 {{--                                <a href="{{ route('address.delete'),  $address->id }}"><button class="btn btn-outline- " style="color: red">{{ __('home.delete') }}</button></a>--}}
                             </div>
                         </div>
-                        <p class="mb-1">{{ __('home.address') }}:{{ ($address->address_detail) }}, {{ ($address->location) }},
-                            {{ ($address->province) }}, {{ ($address->city) }}</p>
-                        <small>{{ __('home.phone number') }}: {{ ($address->phone) }}</small>
+                        <div class="d-flex justify-content-between">
+                            <div class="d-flex"><span class="mb-1">{{ __('home.phone number') }}: &nbsp;</span>
+                                <div class="name-address-book"> {{ ($address->phone) }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <div class="d-flex"><span class="mb-1">{{ __('home.address') }}:&nbsp;</span>
+                                <div class="name-address-book">  {{ ($address->address_detail) }}, {{ ($address->location) }},
+                                    {{ ($address->province) }}, {{ ($address->city) }}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
