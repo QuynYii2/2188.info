41ff039951b1bff0403e73a810da4d14b5118cde
dainqd <ngodaixtp@gmail.com>
2024-01-31 10:27:07 +0700

    Fix login register member logistic.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -239,6 +239,95 @@
             </div>
         </div>
 
+        @if($member->name == 'LOGISTIC')
+            <div class="bg-white">
+                @include('frontend.pages.member.header_member_only_main')
+                <div class="row m-0">
+                    <div class="col-md-6 border">
+                        <div class="row">
+                            <div class="col-md-12 border" style="border-right: 1px solid white!important">
+                                <div class="mt-2">
+                                    <h5 class="mb-3">
+                                        {{ ($company->name) }}
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="row p-2">
+                                <div class="col-md-6">
+                                    <div class="mt-2">
+                                        <div class="mb-3 size"><b>{{ __('home.Company code') }}
+                                                : </b> {{ ($company->code_business) }}</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mt-2">
+                                        <div class="mb-3 size"><b>{{ __('home.Elite enterprise') }}
+                                                : </b> {{ ($company->member) }}</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mt-2">
+                                        <div class="mb-3 size"><b>{{ __('home.Membership classification') }}
+                                                : </b> {{ ($company->member) }}</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mt-2">
+                                        <div class="mb-3 size"><b>{{ __('home.Customer rating score') }}: </b></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 border">
+                        <div class="row">
+                            <div class="col-md-12 border" style="border-left: 1px solid white!important">
+                                <div class="mt-2">
+                                    <h5 class="mb-3">{{ __('home.Specified products') }}</h5>
+                                </div>
+                            </div>
+                            @php
+                                $listCategory = $company->category_id;
+                                $arrayCategory = explode(',', $listCategory);
+                            @endphp
+                            <div class="col-12">
+                                <div class="row">
+                                    @foreach($arrayCategory as $itemArrayCategory)
+                                        @php
+                                            $category = \App\Models\Category::find($itemArrayCategory);
+                                        @endphp
+                                        @if($category)
+                                            <div class="col-md-6">
+                                                <div class="mt-2 d-flex">
+                                                    <a href="{{route('category.show', $category->id)}}" class="mb-3 size">
+                                                        @if(locationHelper() == 'kr')
+                                                            {{ ($category->name_ko) }}
+                                                        @elseif(locationHelper() == 'cn')
+                                                            {{ ($category->name_zh) }}
+                                                        @elseif(locationHelper() == 'jp')
+                                                            {{ ($category->name_ja) }}
+                                                        @elseif(locationHelper() == 'vi')
+                                                            {{ ($category->name_vi) }}
+                                                        @else
+                                                            {{ ($category->name_en) }}
+                                                        @endif
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        @endif
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        @if($member->name == 'TRUST')
+
+        @endif
+
         <div class="section margin-layout-index margin-top-layout container-fluid mt-3">
             <div class="d-flex justify-content-between align-items-center">
                 <h3 class="content-products">{{ __('home.New Products') }}</h3>
